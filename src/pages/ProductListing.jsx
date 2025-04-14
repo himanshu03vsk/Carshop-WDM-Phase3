005762040276b1fc6740acfa45c93dcba0e1a371
andmrqzv <83743411+andmrqzv@users.noreply.github.com>
2025-04-13 22:58:16 -0500

    Product Listing Functionality implemented

--- src/pages/ProductListing.jsx
+++ src/pages/ProductListing.jsx
@@ -1,21 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryList from '../components/CategoryList';
 import CarSearch from '../components/CarSearch';
-
-
-
+import PartList from '../components/PartList';
 
 const ProductListing = () => {
+    const [selectedCategory, setSelectedCategory] = useState(null);
+    const [carFilters, setCarFilters] = useState({ make: '', model: '', year: '' });
+  
+    const handleSearch = (filters) => {
+        setSelectedCategory(null); 
+        setCarFilters(filters);  
+      };
+  
     return (
-
-        <div>
-            <h2>This the ProductListing page</h2>
-            <CategoryList />
-            <CarSearch />
+      <div>
+        <div style={{ display: 'flex', justifyContent: 'center', margin: '20px 0' }}>
+        <CarSearch onSearch={handleSearch} />
+      </div>
+  
+        <div style={{ display: 'flex', width: '90%', margin: '0 auto' }}>
+          <div style={{ flex: '0 0 30%' }}>
+          <CategoryList
+            onCategorySelect={(cat) => {
+                setCarFilters({ make: '', model: '', year: '' }); 
+                setSelectedCategory(cat);                
+            }}
+            />
+          </div>
+          <div style={{ flex: '0 0 70%' }}>
+            <PartList category={selectedCategory} carFilters={carFilters} />
+          </div>
         </div>
+      </div>
     );
-};
-
-
-export default ProductListing;
-
+  };
+  
+  export default ProductListing;
