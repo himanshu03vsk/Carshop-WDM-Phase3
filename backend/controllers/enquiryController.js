a6413dc62cc41e52ba596a6f77bf8c6ffe140a3b
himanshu03vsk <58275123+himanshu03vsk@users.noreply.github.com>
2025-04-17 00:32:38 -0500

    feat: add create enquiry functionality to enquiry controller and routes

--- backend/controllers/enquiryController.js
+++ backend/controllers/enquiryController.js
@@ -10,3 +10,18 @@ exports.getAllEnquiries = async (req, res) => {
         res.status(500).json({ message: 'Server error', error: error.message });
     }
 }
+
+
+
+
+
+exports.createEnquiry = async (req, res) => {
+    try {
+        const { name, email, message_subject, message, phone,  } = req.body;
+        const newEnquiry = await Enquiry.create({ name, message_subject, phone, email, message });
+        res.status(201).json(newEnquiry);
+    } catch (error) {
+        console.error('Error creating enquiry:', error.message);
+        res.status(500).json({ message: 'Server error', error: error.message });
+    }
+}
--- backend/routes/enquiryRoutes.js
+++ backend/routes/enquiryRoutes.js
@@ -5,6 +5,7 @@ const EnquiryController = require('../controllers/enquiryController');
 
 
 router.get('/', EnquiryController.getAllEnquiries); // Get all enquiries
+router.post('/', EnquiryController.createEnquiry); // Create a new enquiry
 
 
 module.exports = router;
